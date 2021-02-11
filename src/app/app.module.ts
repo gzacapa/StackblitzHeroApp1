b77db666239ad96c91a276d990685032cd2960aa
gzacapa <g.zacapa@hotmail.com>
2021-02-10 19:25:29 -0600

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,8 @@ import { ProductAlertsComponent } from "./product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from "./product-details/product-details.component";
 import { CartService } from "./cart.service";
 import { CartComponent } from "./cart/cart.component";
-import { HttpClientModule } from '@angular/common/http';
-import { ShippingComponent } from './shipping/shipping.component';
+import { HttpClientModule } from "@angular/common/http";
+import { ShippingComponent } from "./shipping/shipping.component";
 
 @NgModule({
   imports: [
@@ -21,7 +21,8 @@ import { ShippingComponent } from './shipping/shipping.component';
     RouterModule.forRoot([
       { path: "", component: ProductListComponent },
       { path: "products/:productId", component: ProductDetailsComponent },
-      { path: "cart", component: CartComponent }
+      { path: "cart", component: CartComponent },
+      { path: "shipping", component: ShippingComponent }
     ])
   ],
   declarations: [
